f8b7738ca38ba68e7800351c65c3f6573dc2e7bd
git-chad <tobimocc@gmail.com>
2024-05-19 18:14:10 -0300

    chore(dev): container + sections init

---
+++ src/components/ui/container.tsx
@@ -0,0 +1,16 @@
+import { type FC, type ReactNode } from 'react';
+
+interface ContainerProps {
+  children: ReactNode;
+  className?: string;
+}
+
+const Container: FC<ContainerProps> = ({ children, className = '' }) => {
+  return (
+    <div className={`px-10 md:px-0 xl:max-w-[1068px] 2xl:max-w-[1280px] 3xl:max-w-[1432px] mx-auto ${className}`}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
