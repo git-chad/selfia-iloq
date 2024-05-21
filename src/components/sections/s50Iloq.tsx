9c88c187a8b85528959756c5003ee47e285231b5
Juan Troncoso <troncosojuanbernabe@gmail.com>
2024-05-21 19:32:20 -0300

    chore: Update index.astro to include S50Iloq component

---
+++ src/components/sections/s50Iloq.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const S50Iloq: React.FC = () => {
+    return (
+        <section className="grid grid-cols-12 gap-5">
+            <div className="flex flex-row col-span-12 justify-between items-center">
+                <h1 className=" col-span-6 text-start font-semibold text-[4rem] w-1/2">Autoalimentadas. Sin baterías, ni cables.</h1>
+                <div className=" items-center bg-radial-custom rounded-full w-max py-2 px-8 shadow-md border-white border-2 mb-8">
+                    <span className="text-black text-sm font-semibold leading-none tracking-tighter">iLOQ 5 Series</span>
+                </div>
+            </div>
+
+            <div className="flex flex-row col-span-12 justify-between items-center">
+                <p></p>
+                <img></img>
+            </div>
+
+        </section>
+    )
+}
+
+export default S50Iloq;
