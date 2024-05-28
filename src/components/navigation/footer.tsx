2508dc72e8674edbd3cd7d03cc4f34e3a8331396
Juan Troncoso <troncosojuanbernabe@gmail.com>
2024-05-28 19:41:28 -0300

    chore: Update footer component styling and content

--- src/components/navigation/footer.tsx
+++ src/components/navigation/footer.tsx
@@ -1,49 +1,44 @@
 import { ChevronsUp, Contact, Facebook, Instagram, Linkedin } from 'lucide-react';
 import React from 'react';
+import Container from '../ui/container';
 
 const Footer: React.FC = () => {
     return (
-        <footer className="grid grid-cols-12 gap-5 bg-faded-black items-center relative">
+        <footer className=" bg-faded-black items-center relative pt-16 pb-4">
 
-
-            <h1 className=" col-span-12  text-center text-white font-semibold text-[4rem] leading-none tracking-tighter my-16">SELFIA</h1>
-
-            <div className="flex flex-row col-span-12 justify-around items-center ">
-                <div className='flex flex-col gap-3'>
-                    <a href='mailTo:selfia@email.com' className='text-4xl text-white font-semibold '> selfia@email.com</a>
-                    <div className=' h-px bg-white'></div>
-                </div>
-                <div className="flex flex-row gap-2">
-                    <Contact size={80} color='white' />
-                    <div className='flex flex-col gap-2'>
-                        <a href='tel:+1234567890' className='text-white text-2xl font-semibold'> +1234567890</a>
-                        <div className=' h-px bg-white'></div>
-                        <a href="maps:location" className=' text-gray-500 text-lg font-semibold'>Colombia / Bogotá</a>
+            <Container className='grid grid-cols-12 gap-5'>
+      
+            <div className='flex flex-col gap-2 text-white/80 col-span-2 col-start-1'>
+                <h1 className='text-3xl text-white text-semibold mb-2'>Secciones</h1>
+                <a href='#about'>Nosotros</a>
+                <a href='#iloq5'>ILOQ 5</a>
+                <a href='#iloq50'>ILOQ 50</a>
+                <a href='#solutions'>Soluciones</a>
+                <a href='#sust'>Sustentabilidad</a>
+                <a href='#contact'>Contacto</a>
+            </div>
+            
+            <div className='flex flex-col gap-4 items-start text-white/80 col-span-2 col-start-4'>
+                <h1 className='text-3xl text-white text-semibold'>Redes</h1>
+                <div className='flex flex-col gap-2 '>
+                    <a href='' className='flex items-center gap-2 leading-none '><Linkedin/> Linkedin</a>
+                    <a href='' className='flex items-center gap-2 leading-none '><Facebook/>Facebook</a>
+                    <a href='' className='flex items-center gap-2 leading-none '><Instagram/> Instagram</a>
                     </div>
                 </div>
+
+            <div className='flex flex-col gap-2 text-white/80 col-span-2 col-start-11'>
+                <h1 className='text-3xl text-white text-semibold'>Contacto</h1>
+                <a href='mailto:mail@mail.com' className=' text-xl text-selfia-green'>selfia@mail.com</a>
             </div>
 
-            <div className='col-span-10 col-start-2 items-center justify-center h-px bg-white/30'></div>
+            <h1 className='col-span-12 text-[16rem] tracking-tighter font-bold text-white leading-none'>Selfia</h1>
 
-
-            <ChevronsUp className='text-white absolute right-10 bottom-10' size={40} />
-
-            <div className='col-span-12 flex justify-center gap-4'>
-                <div className='p-2 rounded-full border border-gray-500 '>
-                    <Linkedin className='text-white' />
-                </div>
-                <div className='p-2 rounded-full border border-gray-500 '>
-                    <Facebook className='text-white' />
-                </div>
-                <div className='p-2 rounded-full border border-gray-500 '>
-                    <Instagram className='text-white' />
-                </div>
-            </div>
-
-
-            <div className="col-span-12 flex justify-center">
-                <p className="text-center text-sm">© 2021 iLOQ. All rights reserved.</p>
-            </div>
+            <p className='col-span-12 text-center text-white/80'>© 2024 Selfia. All rights reserved.</p>
+            
+            </Container>
+        
+          
         </footer>
     )
 }
